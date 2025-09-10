3385a8e80679c1fb1826336016153ab8b3dc1e1c
SleepyDut <diegoundreiner@gmail.com>
2025-09-10 19:53:15 -0400

    Cambiar guardar -> seleccionar

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -568,7 +568,7 @@ const Profile = () => {
                                 }
                               }}
                             >
-                              Guardar Imagen
+                              Seleccionar Imagen
                             </button>
                           )}
                         </div>
